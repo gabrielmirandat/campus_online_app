140c6fc57508e7572398abde1a9485df0b942d50
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2018-04-20 00:27:41 -0300

    Modificando frontend para ter noticias fakes

--- frontend/src/components/NewsList.js
+++ frontend/src/components/NewsList.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import NewsItem from './NewsItem'
-import '../style/NewsList.css';
+import React, { Component } from 'react';
+import News from './News'
+import testImg from '../icon.png'
 
-const NewsList = ({newsList}) => {
+class NewsList extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="news-list">
-      {newsList.map((newsItem, index) => <NewsItem newsItem={newsItem} key={index} />)}
-    </div>
-  );
-};
+    this.news = [];
+    this.addNews( new News( null,
+                            "Microsoft anuncia nova logo",
+                            "Mudança total de identidade visual",
+                            "20/04/2018 - 11h10",
+                            "<p>De forma completamente inesperada, a Microsoft anunciou uma nova identidade visual.</p>\
+                            <p>Completamente diferente de tudo que já foi feito antes, essa nova identidade está mais minimalista.</p>\
+                            <p>A controvérsia foi grande mas designers do mundo inteiro concordam que com certeza representa a nova era na qual a Microsoft está passando.</p>",
+                            0, testImg) );
+    this.addNews( new News( null,
+                            "Campus Online agora também em aplicativo",
+                            "Novo veículo de comunicação mais fácil para os usuários",
+                            "20/04/2018 - 9h30",
+                            "<p>Os alunos de Jornalismo da Faculdade de Comunicação (FAC) em parceria com alunos de Engenharia e Ciência da Computação do Departamento de Ciência da Computação (CIC) liberaram hoje o acesso ao novo aplicativo para acesso mais rápido às notícias do Campus Online.</p>\
+                            <p>Esse aplicativo não precisa ser instalado através de uma loja como aplicativos mais tradicionais, afirmam os estudantes. Basta salvar o site em seu celular e o acesso será idêntico ao de um aplicativo normal.</p>\
+                            <p>E o que vocês acharam disso? Promissor? Vocês vão usar? Nós aqui da redação adoramos a ideia e todos os nossos parentes já estão usando!</p>") );
+  }
+
+  addNews( news ) {
+    this.news.push( news );
+    this.news.sort( News.compare );
+  }
+
+  render() {
+    return(
+      <div className="newsList">{this.news.map( (value, index) => { return value.render(index); } )}</div>
+    );
+  }
+}
 
 export default NewsList;
